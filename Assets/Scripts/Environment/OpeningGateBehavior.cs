de0a54e7b42888ff6ff54834a5e3f28965f78da8
sTarBlakout <46494422+sTarBlakout@users.noreply.github.com>
2020-05-02 21:25:23 +0200

    Gate Finished

--- Assets/Scripts/Environment/OpeningGateBehavior.cs
+++ Assets/Scripts/Environment/OpeningGateBehavior.cs
@@ -1,23 +1,37 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using TDH.Player;
 
 public class OpeningGateBehavior : MonoBehaviour
 {
     [Header("Components")]
     [SerializeField] Transform standingPoint = null;
     [SerializeField] Transform firstGateTransformPoint = null, secondGateTransformPoint = null;
+    [SerializeField] ParticleSystem[] paticlesOnOpening = null;
+
     [Header("Values")]
     [SerializeField] float openingSpeed = 0.1f;
     [SerializeField] float openingTime = 2f;
+    [SerializeField] float delayAfterActivating = 1f;
+
     [Header("UI")]
     [SerializeField] GameObject openButton = null;
+
+    private PlayerMover playerMover = null;
 
     private float timeToStopOpening = 0.0f;
 
     private bool isOpening = false;
 
+    private void Awake() 
+    {
+        playerMover = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMover>();
+    }
+
     private void Start() 
     {
         openButton.SetActive(false);
+        OpeningParticlePlay(false);
     }
 
     private void FixedUpdate() 
@@ -31,15 +45,39 @@ public class OpeningGateBehavior : MonoBehaviour
         firstGateTransformPoint.Translate(-transform.right * openingSpeed, Space.World);
         secondGateTransformPoint.Translate(transform.right * openingSpeed, Space.World);
         if (Time.time > timeToStopOpening)
+        {
             isOpening = false;
+            OpeningParticlePlay(false);
+        }
     }
 
-    public void StartOpening()
+    private void OpeningParticlePlay(bool play)
+    {
+        foreach (ParticleSystem particle in paticlesOnOpening)
+        {
+            if (play)
+                particle.Play();
+            else
+                particle.Stop();
+        }
+    }
+
+    public void StartOpeningSequence()
     {
         transform.GetComponent<BoxCollider>().enabled = false;
         openButton.SetActive(false);
+        if (playerMover != null)
+            playerMover.ForceMove(standingPoint.position, standingPoint.rotation, "Activate");
+        StartCoroutine(StartOpeningCor());
+    }
+
+    private IEnumerator StartOpeningCor()
+    {
+        yield return new WaitForSeconds(delayAfterActivating);
         timeToStopOpening = Time.time + openingTime;
         isOpening = true;
+        OpeningParticlePlay(true);
+        playerMover.AllowMove();
     }
 
     private void OnTriggerEnter(Collider other) 
